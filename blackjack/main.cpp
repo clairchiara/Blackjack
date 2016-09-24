e6cca47b6e8a05c1405c6d99f190cfb7dbc47ba9
clairchiara <clair.chiara@gmail.com>
2016-09-24 18:29:47 +0100

    Empty main

---
+++ blackjack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  blackjack
+//
+//  Created by Clair Marchesani on 24/09/2016.
+//  Copyright © 2016 Clair Marchesani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
