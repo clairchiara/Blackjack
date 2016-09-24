3af7071c69a26d8bab5127d36c91fe431efc9365
clairchiara <clair.chiara@gmail.com>
2016-09-24 18:30:22 +0100

    Hand class

---
+++ blackjack/hand.cpp
@@ -0,0 +1,42 @@
+//
+//  hand.cpp
+//  blackjack
+//
+//  Created by Clair Marchesani on 24/09/2016.
+//  Copyright © 2016 Clair Marchesani. All rights reserved.
+//
+
+#include "hand.hpp"
+
+unsigned int Hand::value() const {
+	unsigned int aces{0};
+	unsigned int sum{0};
+	for (const Card* const card : cards) {
+		if (card->number == ACE) aces++;
+		sum += card->number;
+		if (sum > 21) {
+			while (aces > 0 and sum > 21) {
+				aces--;
+				sum -= 10;
+			}
+		}
+	}
+	return sum;
+};
+
+bool Hand::bust() const {
+	if (value() > 21) return true;
+	return false;
+};
+
+void Hand::addCard(const Card* const card) {
+	cards.push_back(card);
+};
+
+void Hand::empty() {
+	cards.clear();
+};
+
+std::vector<const Card*> Hand::getCards() const {
+	return cards;
+};
---
+++ blackjack/hand.hpp
@@ -0,0 +1,27 @@
+//
+//  hand.hpp
+//  blackjack
+//
+//  Created by Clair Marchesani on 24/09/2016.
+//  Copyright © 2016 Clair Marchesani. All rights reserved.
+//
+
+#ifndef hand_hpp
+#define hand_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "card.hpp"
+
+class Hand {
+private:
+	std::vector<const Card*> cards;
+public:
+	unsigned int value() const;
+	bool bust() const;
+	void addCard(const Card* const card);
+	void empty();
+	std::vector<const Card*> getCards() const;
+};
+
+#endif /* hand_hpp */
